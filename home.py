eeac81a2456344aadea208866db124c4ed16f143
DEBDUT CHOWDHURY <debdut.chowdhury.official@gmail.com>
2025-02-02 00:41:19 +0530

    Added loom video in the home page

--- home.py
+++ home.py
@@ -6,7 +6,14 @@ def home_page():
     st.write('Welcome to the Network Anomaly Detection App!')
 
     st.markdown("""
-    <div style="position: relative; padding-bottom: 64.98194945848375%; height: 0;">
-        loom video
+    <div style="position: relative; padding-bottom: 56.22254758418741%; height: 0;">
+                <iframe src="https://www.loom.com/embed/9a92381c60ef4ccd8e809a21baebb112?sid=5978733f-b424-46c6-a616-0591ec02a269" 
+                    frameborder="0" 
+                    webkitallowfullscreen 
+                    mozallowfullscreen 
+                    allowfullscreen 
+                    style="position: absolute; 
+                    top: 0; left: 0; width: 100%; height: 100%;">
+                    </iframe>
     </div>
     """, unsafe_allow_html=True)
